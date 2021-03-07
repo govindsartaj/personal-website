8465e10973f5d9971264e69304c5040d48b7d5ea
Govind Brahmanyapura <brahmany@grinnell.edu>
2021-03-07 15:15:18 -0600

    added image

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,16 @@
+import Image from 'next/image'
 
 export default function Home({ darkMode }) {
   return (
-    <div className="dark:bg-darkbg h-screen">
-      <div className="pt-16 max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 dark:text-white text-gray-900">
-        Home
+    <div className="h-screen dark:bg-darkbg">
+      <div className="px-4 pt-16 mx-auto text-gray-900 max-w-7xl sm:px-6 lg:px-8 dark:text-white">
+        <div class="rounded-full h-24 w-24 flex items-center justify-center bg-darkbg">
+          <Image src='https://github.com/govindsartaj/personal-website/blob/master/assets/govind.jpg'
+            alt="Picture of the author"
+            width={500}
+            height={500}
+          />
+        </div>
       </div>
     </div>
   )
