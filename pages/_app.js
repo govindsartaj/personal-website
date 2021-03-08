fa59647e41c1dc7fc0b8c617b3d6f35e17aef83b
Govind Brahmanyapura <brahmany@grinnell.edu>
2021-03-08 17:13:07 -0600

    fix bug

--- pages/_app.js
+++ pages/_app.js
@@ -6,11 +6,13 @@ import "../styles/globals.css";
 function MyApp({ Component, pageProps }) {
   const [darkMode, setDarkMode] = useState(false);
 
-  // First we get the viewport height and we multiple it by 1% to get a value for a vh unit
-  let vh = window.innerHeight * 0.01;
-  // Then we set the value in the --vh custom property to the root of the document
-  document.documentElement.style.setProperty("--vh", `${vh}px`);
-  
+  if (document && window) {
+    // First we get the viewport height and we multiple it by 1% to get a value for a vh unit
+    let vh = window.innerHeight * 0.01;
+    // Then we set the value in the --vh custom property to the root of the document
+    document.documentElement.style.setProperty("--vh", `${vh}px`);
+  }
+
   useEffect(() => {
     if (
       localStorage.theme === "dark" ||
