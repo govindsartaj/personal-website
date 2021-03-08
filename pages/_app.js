1c9773880b366254f146f6ccac992676efe40345
Govind Brahmanyapura <brahmany@grinnell.edu>
2021-03-08 17:04:52 -0600

    fix height to account for mobile browser ui

--- pages/_app.js
+++ pages/_app.js
@@ -1,35 +1,43 @@
 import "tailwindcss/tailwind.css";
-import { useState, useEffect } from 'react'
-import Navbar from '../components/Navbar'
-import '../styles/globals.css'
+import { useState, useEffect } from "react";
+import Navbar from "../components/Navbar";
+import "../styles/globals.css";
 
 function MyApp({ Component, pageProps }) {
   const [darkMode, setDarkMode] = useState(false);
 
-
+  // First we get the viewport height and we multiple it by 1% to get a value for a vh unit
+  let vh = window.innerHeight * 0.01;
+  // Then we set the value in the --vh custom property to the root of the document
+  document.documentElement.style.setProperty("--vh", `${vh}px`);
+  
   useEffect(() => {
-    if (localStorage.theme === 'dark' || (!('theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)) {
-      setDarkMode(true)
+    if (
+      localStorage.theme === "dark" ||
+      (!("theme" in localStorage) &&
+        window.matchMedia("(prefers-color-scheme: dark)").matches)
+    ) {
+      setDarkMode(true);
     } else {
-      setDarkMode(false)
+      setDarkMode(false);
     }
-  }, [])
-
+  }, []);
 
   const toggleDarkMode = (darkModeActive) => {
     if (darkModeActive) {
-      localStorage.theme = 'dark'
+      localStorage.theme = "dark";
     } else {
-      localStorage.theme = 'light'
+      localStorage.theme = "light";
     }
-    setDarkMode(darkModeActive)
-  }
-  
+    setDarkMode(darkModeActive);
+  };
 
-  return <div className={darkMode ? 'dark' : ''}>
-  <Navbar setDarkMode={toggleDarkMode} darkMode={darkMode} />
-  <Component {...pageProps} darkMode={darkMode} />
-  </div>
+  return (
+    <div className={darkMode ? "dark" : ""}>
+      <Navbar setDarkMode={toggleDarkMode} darkMode={darkMode} />
+      <Component {...pageProps} darkMode={darkMode} />
+    </div>
+  );
 }
 
-export default MyApp
+export default MyApp;
